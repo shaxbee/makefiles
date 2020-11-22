109e430486f78271940ecdfce5d6c769f864beb2
Zbigniew Mandziejewicz <shaxbee@gmail.com>
2020-11-22 14:34:00 +0400

    refactor: move scripts to makefiles, add kubectl installer

--- skaffold.mk
+++ skaffold.mk
@@ -2,6 +2,7 @@ ifndef include_skaffold_mk
 _include_skaffold_mk := 1
 
 include makefiles/shared.mk
+include makefiles/kubectl.mk
 
 SKAFFOLD := bin/skaffold
 SKAFFOLD_VERSION ?= 1.16.0
@@ -14,33 +15,34 @@ $(SKAFFOLD):
 
 deploy: deploy-skaffold
 
-.PHONY: clean-skaffold build-skaffold deploy-skaffold run-skaffold deploy-skaffold dev-skaffold
+.PHONY: clean-skaffold build-skaffold deploy-skaffold run-skaffold dev-skaffold debug-skaffold
 
-clean-skaffold: $(SKAFFOLD) ## Clean Skaffold
+clean-skaffold build-skaffold deploy-skaffold run-skaffold dev-skaffold debug-skaffold: $(SKAFFOLD) $(KUBECTL)
+clean-skaffold build-skaffold deploy-skaffold run-skaffold dev-skaffold debug-skaffold: export PATH := $(shell pwd)/bin:$(PATH)
+
+clean-skaffold: ## Clean Skaffold
 	$(info $(_bullet) Cleaning <skaffold>)
 	! kubectl config current-context &>/dev/null || \
-	PATH=bin:$(PATH) $(SKAFFOLD) delete
+	$(SKAFFOLD) delete
 
-build-skaffold: $(SKAFFOLD) ## Build artifacts with Skaffold
+build-skaffold: ## Build artifacts with Skaffold
 	$(info $(_bullet) Building artifacts with <skaffold>)
-	PATH=bin:$(PATH) $(SKAFFOLD) build
+	$(SKAFFOLD) build
 
-deploy-skaffold: $(SKAFFOLD) build-skaffold ## Deploy artifacts with Skaffold
+deploy-skaffold: build-skaffold ## Deploy artifacts with Skaffold
 	$(info $(_bullet) Deploying with <skaffold>)
 	$(SKAFFOLD) build -q | $(SKAFFOLD) deploy --force --build-artifacts -
 
-run-skaffold: $(SKAFFOLD) ## Run with Skaffold
+run-skaffold: ## Run with Skaffold
 	$(info $(_bullet) Running stack with <skaffold>)
 	$(SKAFFOLD) run --force
 
-dev-skaffold: $(SKAFFOLD) ## Run in development mode with Skaffold
+dev-skaffold: ## Run in development mode with Skaffold
 	$(info $(_bullet) Running stack in development mode with <skaffold>)
 	$(SKAFFOLD) dev --force --port-forward
 
-debug-skaffold: $(SKAFFOLD) ## Run in debugging mode with Skaffold
+debug-skaffold: ## Run in debugging mode with Skaffold
 	$(info $(_bullet) Running stack in debugging mode with <skaffold>)
 	$(SKAFFOLD) debug --force --port-forward
-
-clean-skaffold build-skaffold deploy-skaffold run-skaffold dev-skaffold debug-skaffold: export PATH := $(shell pwd)/bin:$(PATH)
 
 endif
